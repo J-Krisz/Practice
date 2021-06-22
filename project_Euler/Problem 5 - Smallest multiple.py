34f3bec8e340a67c123e768fe840b645983f8af3
J-Krisz <jkristi4n@gmail.com>
2021-06-22 12:21:15 +0100

    brute_force solution

--- project_Euler/Problem 5 - Smallest multiple.py
+++ project_Euler/Problem 5 - Smallest multiple.py
@@ -1,3 +1,10 @@
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder. 
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20? 
-# The page has been left unattended for too long and that link/button is no longer active. Please refresh the page. 
+
+## Brute force
+from itertools import count
+for i in count(20):
+    if all(map(lambda x: i % x == 0, range(1, 21))):
+        print(i)
+        break
+
