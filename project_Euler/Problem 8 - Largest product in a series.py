9362d5f7611f93b52e5e1099e756e55c39904134
J-Krisz <jkristi4n@gmail.com>
2021-06-27 22:21:46 +0100

    not correct

--- project_Euler/Problem 8 - Largest product in a series.py
+++ project_Euler/Problem 8 - Largest product in a series.py
@@ -1,5 +1,6 @@
 # The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832. 
-# 
+# Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
+series = """
 73167176531330624919225119674426574742355349194934
 96983520312774506326239578318016984801869478851843
 85861560789112949495459501737958331952853208805511
@@ -19,6 +20,19 @@
 07198403850962455444362981230987879927244284909188
 84580156166097919133875499200524063689912560717606
 05886116467109405077541002256983155200055935729725
-71636269561882670428252483600823257530420752963450 
-# Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product? 
-# The page has been left unattended for too long and that link/button is no longer active. Please refresh the page. 
+71636269561882670428252483600823257530420752963450
+"""
+
+product = 1
+largest_prod = 0
+
+nums = [int(i) for i in series if i != "\n"]
+
+for num in range(len(nums) + 1):
+    for j in nums[num:num+13]:
+        product *= j
+
+    if product > largest_prod:
+        largest_prod = product
+
+print(largest_prod)
