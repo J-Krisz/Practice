f99211559981b57e66eedb7fa5d3e387b29e5082
J-Krisz <jkristi4n@gmail.com>
2021-07-12 20:13:32 +0100

    strings

---
+++ Python Workout 50 essential excercises/strings/exercise_05_b3.py
@@ -0,0 +1,15 @@
+#############
+# Pig Latin #
+#############
+
+# Check to see if the word contains two different vowels. Of ot does, we dont't
+# move the first letter to the end. Because the word "wine" contains two
+# different vowels ("i" and "e"), we'll add "way" to the end of it, giving us
+# "wineway". By contrastm the word "wind" contains only one vowel, so we would
+# move the firdt letter to the end and add "ay", rendering it "indway". How
+# would you check for two different vowels in the word? (HINT: sets can come in
+# handy here.)
+
+def translate():
+
+    pass
