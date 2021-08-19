2631f6ae7394672d7dc8753fc90e5c2a5b19810b
J-Krisz <jkristi4n@gmail.com>
2021-08-19 21:50:09 +0100

    re-organise

---
+++ Python/Python Workout 50 essential excercises/strings/exercise_06_b2.py
@@ -0,0 +1,13 @@
+######################
+# Pig Latin Sentence #
+######################
+
+# Write a function that transposes a list of strings, in which each string
+# contains multiple words separated by white space. Specifically, it should
+# perforn ib such a way that if you were to pass the list ['abc def gih',
+# 'jkl mno pqr', 'stu vwx yz'] to the function, it would return ['abc jkl stu',
+# 'def mno vwx', 'ghi pqr yz']
+
+def transpose():
+
+
---
+++ Python/Python Workout 50 essential excercises/strings/exercise_08.py
@@ -0,0 +1,10 @@
+###########
+# Strings #
+###########
+
+
+# In this exercise, you'll explore the idea by writing a function, strsort(),
+# that takes a single string as its input and returns a string. The returned
+# string should contain the same characters as the input, except that its
+# characters should be stored in order, from the lowest Unicode value to the
+# highest
