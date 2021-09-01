2bbdce9e4e3ea090654d2970727eeea08fcd1e16
J-Krisz <jkristi4n@gmail.com>
2021-09-01 22:51:43 +0100

    lists

---
+++ Python/Python Workout 50 essential excercises/lists/exercise_09.py
@@ -0,0 +1,7 @@
+####################
+# Lists and Tuples #
+####################
+
+
+# Write a function, firstlast(), that takes sequence (string, list or tuple)
+# and returns the first and last elements of that sequence,
