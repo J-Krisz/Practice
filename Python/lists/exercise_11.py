017f8814f962b0405c0ccc568ffa4a3daf913992
J-Krisz <jkristi4n@gmail.com>
2021-09-15 20:48:48 +0100

    e11

--- Python/lists/exercise_11.py
+++ Python/lists/exercise_11.py
@@ -1,1 +1,7 @@
-PEOPLE = [{'first':
+PEOPLE = [
+{'first': 'Reuven', 'last': 'Lrner', 'email': 'reuven@lerner.co.li'},
+{'first': 'Donald', 'last': 'Trump', 'email': 'president@whitehouse.gov'},
+{'first': 'Vladimir', 'last': 'Putin', 'email': 'president@kremvax.ru'}
+]
+
+
