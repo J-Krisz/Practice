a38d3476dbc784cb56e3c8fbe068d8d7b83fe733
J-Krisz <jkristi4n@gmail.com>
2021-09-30 16:38:53 +0100

    code_wars

---
+++ Python/Code_Wars/6_kyu/(a+b)^n.py
@@ -0,0 +1,17 @@
+# Create a function that takes a integer n and returnsthe formule for (a+b)^n
+# as a string.
+
+    """
+    formule(0) --> '1'
+    formula(1) --> 'a+b'
+    formula(2) --> 'a^2+2ab+b^2'
+    formula(-2) --> '1/(a^2+2ab+b^2)'
+    formula(3) --> 'a^3+3a^2b+3ab^2+b^3'
+    formula(5) --> 'a^5+5a^4b+10a^2b^3+5ab^4+b^5'
+    """
+
+# Important notes:
+
+    # Your string may not have spaces so you can't do this: 
+
+
